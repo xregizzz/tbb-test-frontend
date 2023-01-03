c6a6406cb9aee8d341ef1ca2a6a394c0a8c67376
Regis T. Silveira <xregizzz@gmail.com>
2023-01-03 16:19:47 -0300

    create: adding assets and icons

---
+++ tbb-test-regis/src/assets/icons/menu.jsx
@@ -0,0 +1,52 @@
+export const MenuIcon = () => {
+  return (
+    <svg
+      id="ic.menu"
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+    >
+      <rect
+        id="Rectangle_27"
+        data-name="Rectangle 27"
+        width="24"
+        height="24"
+        fill="none"
+      />
+      <g
+        id="Rectangle_56"
+        data-name="Rectangle 56"
+        transform="translate(6 16)"
+        fill="#fff"
+        stroke="#707070"
+        stroke-width="1"
+      >
+        <rect width="12" height="2" rx="1" stroke="none" />
+        <rect x="0.5" y="0.5" width="11" height="1" rx="0.5" fill="none" />
+      </g>
+      <g
+        id="Rectangle_57"
+        data-name="Rectangle 57"
+        transform="translate(6 11)"
+        fill="#fff"
+        stroke="#707070"
+        stroke-width="1"
+      >
+        <rect width="12" height="2" rx="1" stroke="none" />
+        <rect x="0.5" y="0.5" width="11" height="1" rx="0.5" fill="none" />
+      </g>
+      <g
+        id="Rectangle_58"
+        data-name="Rectangle 58"
+        transform="translate(6 6)"
+        fill="#fff"
+        stroke="#707070"
+        stroke-width="1"
+      >
+        <rect width="12" height="2" rx="1" stroke="none" />
+        <rect x="0.5" y="0.5" width="11" height="1" rx="0.5" fill="none" />
+      </g>
+    </svg>
+  );
+};
