4b67d0cbca7fee46a91f2962813ae53166887334
Regis T. Silveira <xregizzz@gmail.com>
2023-01-03 17:26:48 -0300

    feat: adding header component

--- tbb-test-regis/src/App.js
+++ tbb-test-regis/src/App.js
@@ -1,9 +1,11 @@
 import GlobalStyle from "./styles/global";
+import Header from "./components/Header";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
     </>
   );
 }
---
+++ tbb-test-regis/src/components/Header/styles.js
@@ -0,0 +1,35 @@
+import styled from "styled-components";
+
+export const Container = styled.div`
+  height: 80px;
+  background-color: var(--white);
+  width: 100vw;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  img {
+    height: 79px;
+    width: 195px;
+  }
+
+  .headerLogo {
+    display: flex;
+    justify-content: end;
+    width: 54%;
+  }
+
+  .searchLogo {
+    display: flex;
+    justify-content: center;
+    width: 46%;
+  }
+
+  .searchLogo:hover {
+    cursor: pointer;
+  }
+
+  .headerLogo:hover {
+    cursor: pointer;
+  }
+`;
